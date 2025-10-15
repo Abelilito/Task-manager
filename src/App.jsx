aa6650376f67c13e9324a29568dd42f57729e5ae
Abel Ilito Limoma <ilitoabel@gmail.com>
2025-10-16 01:09:07 +0200

    Feat: add legal notice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { LegalNotice } from "./components/LegalNotice/LegalNotice";
 import { TaskManager } from "./components/TaskManager/TaskManager";
 
 function App() {
   return (
     <>
       <TaskManager />
+      <LegalNotice />
     </>
   );
 }
